94ad4a062f26af256829db74ab9c7a3e2bb4c2fa
MOEP-Robotik <Moepdbg@outlook.de>
2025-12-03 16:15:19 +0100

    1.fertigstellung run2

--- FLL_2025_26/Programm2.py
+++ FLL_2025_26/Programm2.py
@@ -1,3 +1,5 @@
+#SILO IST KACKE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor
 from pybricks.robotics import DriveBase
@@ -9,17 +11,34 @@ HUB = PrimeHub()
 HUB.imu.reset_heading(0)
 BEWEGUNGSMOTOREN = DriveBase(Motor(Port.D, Direction.COUNTERCLOCKWISE), Motor(Port.C), 56, 150)
 BEWEGUNGSMOTOREN.use_gyro(True)
-.
 TOPRIGHT = Motor(Port.A)
 TOPLEFT = Motor(Port.E)
 
 BEWEGUNGSMOTOREN.settings(straight_speed=800)
 
-TOPLEFT.run_angle(1000, 120)
 TOPRIGHT.run_angle(1000, 300, wait=False)
+BEWEGUNGSMOTOREN.straight(250)
+
+"""for i in range(0,4):
+    TOPLEFT.run_angle(770, 90)
+    wait(500)
+    TOPLEFT.run_angle(770, -90)"""
+
+BEWEGUNGSMOTOREN.turn(-45)
 BEWEGUNGSMOTOREN.straight(350)
-for i in range(0,5):
-    TOPLEFT.run_angle(1200, -120)
-    TOPLEFT.run_angle(1200, 120)
-BEWEGUNGSMOTOREN.straight(-450)
-TOPRIGHT.run_angle(1000, -300)
+BEWEGUNGSMOTOREN.turn(45)
+BEWEGUNGSMOTOREN.straight(220)
+TOPRIGHT.run_angle(1000, -280, wait=False)
+BEWEGUNGSMOTOREN.turn(40)
+BEWEGUNGSMOTOREN.straight(180)
+TOPRIGHT.run_angle(1000, 250)
+BEWEGUNGSMOTOREN.straight(-55)
+BEWEGUNGSMOTOREN.settings(straight_speed=400)
+BEWEGUNGSMOTOREN.turn(-60)
+BEWEGUNGSMOTOREN.settings(straight_speed=800)
+BEWEGUNGSMOTOREN.straight(-100)
+BEWEGUNGSMOTOREN.turn(-69)
+BEWEGUNGSMOTOREN.straight(-300)
+BEWEGUNGSMOTOREN.straight(400)
+BEWEGUNGSMOTOREN.turn(-105)
+BEWEGUNGSMOTOREN.straight(650)
