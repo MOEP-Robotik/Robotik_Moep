90dedf46885602cb4a669c322587a4a08828a598
MOEP-Robotik <Moepdbg@outlook.com>
2026-02-17 10:25:20 +0100

    bla

--- FLL_2025_26/Hubmenue.py
+++ FLL_2025_26/Hubmenue.py
@@ -1,7 +1,7 @@
 from pybricks.tools import hub_menu
 
 # Make a menu to choose a letter. You can also use numbers.
-selected = hub_menu("1", "2", "3", "4", "T")
+selected = hub_menu("1", "2", "3", "4", "5", "6", "T")
 
 # Based on the selection, run a program.
 if selected == "1":
@@ -12,5 +12,9 @@ elif selected == "3":
     import Run2
 elif selected == "4":
     import Run3
+elif selected == "5":
+    import run4
+elif selected == "6":
+    import Run5 
 elif selected == "T":
     import test
