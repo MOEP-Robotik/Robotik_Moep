60b5e2a5c485c1e7408098509f9553153ee16c76
MOEP-Robotik <Moepdbg@outlook.de>
2026-03-01 10:56:39 +0100

    silo anfang

---
+++ FLL_2025_26/silo.py
@@ -0,0 +1,23 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor
+from pybricks.robotics import DriveBase
+from pybricks.parameters import Port, Direction, Axis, Color, Stop
+from pybricks.tools import wait
+
+HUB = PrimeHub()
+
+HUB.imu.reset_heading(0)
+BEWEGUNGSMOTOREN = DriveBase(Motor(Port.D, Direction.COUNTERCLOCKWISE), Motor(Port.C), 56, 150)
+BEWEGUNGSMOTOREN.use_gyro(True)
+TOPRIGHT = Motor(Port.A)
+TOPLEFT = Motor(Port.E)
+
+BEWEGUNGSMOTOREN.settings(straight_speed=800)
+TOPRIGHT.run_angle(-1000, 100)
+BEWEGUNGSMOTOREN.straight(420)
+
+for i in range(4):
+    TOPRIGHT.run_angle(1000, 80)
+    TOPRIGHT.stop()
+    wait(200)
+    TOPRIGHT.run_angle(-1000, 80)
