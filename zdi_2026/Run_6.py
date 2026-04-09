11c34c416fa19af64848b402754dd2cacd778dbb
MOEP-Robotik <Moepdbg@outlook.de>
2026-04-09 12:56:56 +0200

    Dateien für die fehlenden Runs angelegt (Struktur ist wichtig!!!)

---
+++ zdi_2026/Run_6.py
@@ -0,0 +1,22 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.robotics import DriveBase
+from pybricks.parameters import Port, Direction, Axis
+from pybricks.tools import wait, StopWatch
+
+HUB = PrimeHub()
+HUB.imu.reset_heading(0)
+
+DB = DriveBase(
+    Motor(Port.E, Direction.COUNTERCLOCKWISE),
+    Motor(Port.A),
+    62.4,  # Raddurchmesser
+    100 # Spurweite
+)
+DB.use_gyro(True)
+DB.settings(straight_speed=400)
+DB.settings(turn_rate=900)
+VORNE = Motor(port=Port.C)
+HINTEN = Motor(port=Port.D)
+
+HUB.imu.reset_heading(0)
