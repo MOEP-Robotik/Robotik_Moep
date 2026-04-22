acd2edf1d377a77b11d9c4521a1cce5383154e9e
MOEP-Robotik <Moepdbg@outlook.de>
2026-04-22 15:11:48 +0200

    Hubmenü für zdi

---
+++ zdi_2026/Hubmenue.py
@@ -0,0 +1,22 @@
+from pybricks.tools import hub_menu
+
+# Make a menu to choose a letter. You can also use numbers.
+selected = hub_menu("1", "2", "3", "4", "5", "6", "7", "T")
+
+# Based on the selection, run a program.
+if selected == "1":
+    import Run_1
+elif selected == "2":
+    import Run_2
+elif selected == "3":
+    import Run_3
+elif selected == "4":
+    import Run_4
+elif selected == "5":
+    import Run_5
+elif selected == "6":
+    import Run_6
+elif selected == "7":
+    import Run_7
+elif selected == "T":
+    import motortest
