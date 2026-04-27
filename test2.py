a29283b5c7cf4753f91b48933eb04946bf9541e9
MOEP-Robotik <Moepdbg@outlook.de>
2026-04-27 14:09:40 +0200

    fiuewjdk

--- test2.py
+++ test2.py
@@ -15,33 +15,16 @@ DB = DriveBase(
 )
 DB.use_gyro(True)
 DB.settings(straight_speed=650)
-DB.settings(straight_acceleration=400)
+DB.settings(straight_acceleration=100)
 DB.settings(turn_rate=600)
 VORNE = Motor(port=Port.C)
 HINTEN = Motor(port=Port.D)
 
 HUB.imu.reset_heading(0)
 
-wait(300)
-DB.settings(turn_rate=400)
-DB.turn(10)
-DB.straight(-1010)
-DB.arc(164, 189)
-DB.straight(120)
-DB.settings(straight_acceleration=300)
-print(HUB.imu.heading())
-DB.settings(straight_acceleration=150)
-DB.straight(-10)
-for i in range(1,4):
+while True:
     VORNE.run_angle(-1000, 370)
-    DB.straight(-49) #Länge des Steins?? + Toleranz???
+    DB.straight(-48) #Länge des Steins?? + Toleranz???
     print(HUB.imu.heading())
     VORNE.run_angle(1000, 360)
-    DB.straight(-86)
-DB.settings(straight_acceleration=800)
-VORNE.run_angle(-1000, 370)
-DB.straight(-150)
-DB.turn(-80)
-DB.straight(-550)
-DB.turn(-90)
-DB.straight(600)
+    DB.straight(-88)
