6d179f24b474ae3ea2a22f5eb4612ac786d2e04a
MOEP-Robotik <Moepdbg@outlook.de>
2026-04-28 14:59:27 +0200

    Rakete ohne Hebel

--- zdi_2026/Hubmenue.py
+++ zdi_2026/Hubmenue.py
@@ -1,7 +1,7 @@
 from pybricks.tools import hub_menu
 
 # Make a menu to choose a letter. You can also use numbers.
-selected = hub_menu("1", "2", "3", "4", "5", "6", "7", "T")
+selected = hub_menu("1", "2", "3", "4", "5", "6", "7", "8", "T")
 
 # Based on the selection, run a program.
 if selected == "1":
@@ -18,5 +18,7 @@ elif selected == "6":
     import Run_6
 elif selected == "7":
     import Run_7
+elif selected == "8":
+    import Run_6_alt
 elif selected == "T":
     import motortest
