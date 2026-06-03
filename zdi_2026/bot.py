eb76496b9c3ede14898b9d4d656c28c40be77874
MOEP-Robotik <Moepdbg@outlook.de>
2026-06-03 14:44:40 +0200

    anfang bot klasse

---
+++ zdi_2026/bot.py
@@ -0,0 +1,17 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor
+from pybricks.robotics import DriveBase
+from pybricks.parameters import Port, Direction, Axis
+from pybricks.tools import wait, StopWatch
+
+class BOT():
+    __
+    motor_left = Motor(Port.E, Direction.COUNTERCLOCKWISE)
+    motor_right = Motor(Port.D)
+    DB = DriveBase(
+        motor_left, 
+        motor_right, 
+        62.4,
+        104
+        )
+    DB.use_gyro(True)
